83aa06c7a5f236fda13acac5a72888b950652baa
Antonio Pérez <antonioperezgarciamoreno@gmail.com>
2022-02-07 13:28:44 +0100

    mio

---
+++ SGEntregasAntonioPerezDavidMagdaleno/MisComandos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SGEntregasAntonioPerezDavidMagdaleno
+{
+    class MisComandos
+    {
+        public static RoutedUICommand escapeSalir = new RoutedUICommand("accion cuando se pulsa algo", "Pulsar", typeof(MisComandos), new InputGestureCollection() { new KeyGesture(Key.Escape) });
+    }
+}
